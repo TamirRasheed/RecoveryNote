92c175ec398b6b78d831615dbf6d14a9a83401c8
hongjie003 <hongjieli003@gmail.com>
2021-09-12 16:55:02 -0700

    About page routing Setup
    Hook up Ivan's page

--- application/frontend/src/components/About/Members/MembersData.js
+++ application/frontend/src/components/About/Members/MembersData.js
@@ -1,10 +1,12 @@
 import KunchengsPage from './PersonalPage/KunchengsPage';
 import HongjiesPage from './PersonalPage/HongjiesPage';
 import TamirsPage from './PersonalPage/TamirsPage';
+import IvansPage from './PersonalPage/IvansPage';
 
 import KunchengProfilePicture from './../../../assets/About/KunchengsPage/KunchengsImage.jpg';
 import HongjiesProfileImage from './../../../assets/About/HongjiesPage/hongjie_profile.jpg';
 import TamirsProfileImage from './../../../assets/About/TamirsPage/TamirsImage.JPG';
+import DummyProfileImage from './../../../assets/About/dummy-profile-pic.jpg';
 
 const MembersData = [
   {
@@ -22,14 +24,20 @@ const MembersData = [
     pageComponent: <TamirsPage />,
   },
   {
-    id: 'Kuncheng4',
+    id: 'Kuncheng3',
     name: 'Kuncheng',
     imageSource: KunchengProfilePicture,
     route: '/about/kuncheng',
     pageComponent: <KunchengsPage />,
   },
+  {
+    id: 'Ivan4',
+    name: 'Ivan',
+    imageSource: DummyProfileImage,
+    route: '/about/ivan',
+    pageComponent: <IvansPage />,
+  },
 
-  { id: 'Joe3', name: 'Joe3', imageUrl: '', route: '/about/joe3' },
   { id: 'Joe5', name: 'Joe5', imageUrl: '', route: '/about/joe5' },
   { id: 'Joe6', name: 'Joe6', imageUrl: '', route: '/about/joe6' },
 ];
