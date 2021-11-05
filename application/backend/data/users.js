52d1583d5642e196ea6eaa113d2610898147896d
TamirRasheed <Trasheed@mail.sfsu.edu>
2021-11-04 21:39:18 -0700

    backend login setup

--- application/backend/data/users.js
+++ application/backend/data/users.js
@@ -1,14 +1,69 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const users = [
-  {
-    _id: new mongoose.mongo.ObjectId('aaaaaaaaaaaaaaaaaaaaaaa1'),
-    name: 'Joel',
-  },
-  {
-    _id: new mongoose.mongo.ObjectId('aaaaaaaaaaaaaaaaaaaaaaa2'),
-    name: 'Jack',
-  },
-];
+// const users = [
+//   {
+//     _id: new mongoose.mongo.ObjectId('aaaaaaaaaaaaaaaaaaaaaaa1'),
+//     name: 'Joel',
+//   },
+//   {
+//     _id: new mongoose.mongo.ObjectId('aaaaaaaaaaaaaaaaaaaaaaa2'),
+//     name: 'Jack',
+//   },
+// ];
 
-module.exports = users;
+// module.exports = users;
+
+
+import bcrypt from "bcryptjs";
+
+import User from "../models/users";
+import getSignedToken from "../util/signedToken";
+
+async function createUser(payload) {
+  return User.find({ username: payload.username })
+    .exec()
+    .then((user) => {
+      if (user.length > 0) {
+        throw new Error("User already exist");
+      }
+      return bcrypt
+        .hash(payload.password, 10)
+        .then((hashed) => {
+          const newUser = new User({
+            username: payload.username,
+            password: hashed,
+            school: payload.school,
+            Major: payload.major
+          });
+          return newUser.save();
+        })
+        .catch((err) => {
+          throw new Error("All field required");
+        });
+    });
+}
+
+function signInUser(payload) {
+  return User.findOne({ username: payload.usernmae })
+    .exec()
+    .then((user) => {
+      if (!user) {
+        throw new Error("Please enter email or password");
+      } else {
+        return bcrypt
+          .compare(payload.password, user.password)
+          .then((res) => {
+            if (res) {
+              const token = getSignedToken(user._id);
+              return token;
+            } else {
+              throw new Error("Incorrect password or email, try again");
+            }
+          })
+          .catch((err) => {
+            throw new Error("All field required");
+          });
+      }
+    });
+}
+export default { createUser, signInUser };
