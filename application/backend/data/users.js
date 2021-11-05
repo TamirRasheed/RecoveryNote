ff8640a69e79a0750fcd8f871e32970f0a21324a
hongjie003 <hongjieli003@gmail.com>
2021-11-04 23:56:42 -0700

    Update User data model

--- application/backend/data/users.js
+++ application/backend/data/users.js
@@ -4,12 +4,14 @@ const users = [
   {
     _id: new mongoose.mongo.ObjectId('aaaaaaaaaaaaaaaaaaaaaaa1'),
     name: 'Joel',
+    password: '123',
     school: 'sfsu',
     major: 'cs',
   },
   {
     _id: new mongoose.mongo.ObjectId('aaaaaaaaaaaaaaaaaaaaaaa2'),
     name: 'Jack',
+    password: '123',
     school: 'sfsu',
     major: 'cs',
   },
--- application/backend/models/User.js
+++ application/backend/models/User.js
@@ -5,6 +5,18 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  password: {
+    type: String,
+    require: true,
+  },
+  school: {
+    type: String,
+    require: true,
+  },
+  major: {
+    type: String,
+    require: true,
+  },
 });
 
 const User = mongoose.model('user', userSchema);
